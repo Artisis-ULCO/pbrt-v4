88ea27b0c9a2ee0d929a62b16d560b4c37310c3a
Jérôme Buisine <jerome.buisine@outlook.fr>
2022-11-30 13:09:02 +0100

    update run

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
-nspp=50
+nspp=100
 batch=10
+numthreads=8
+build=build
 
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --fixed 1 --folder build/MIS-Equal ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.99 --fixed 1 --folder build/MIS-BRDF ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.01 --fixed 1 --folder build/MIS-Light ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --folder build/MIS-T0-01 --tsallis 0.01 ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./buiil/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --folder build/MIS-T0-5 --tsallis 0.5 ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --folder build/MIS-T1 --tsallis 1 ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --folder build/MIS-T1-5 --tsallis 1.5 ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
-./build/pbrt --spp $nspp --batch $batch --nthreads 8 --alpha 0.5 --folder build/MIS-T2 --tsallis 2 ../pbrt-v4-scenes-P3D/veach-mis/mis_v4.pbrt
+for scene in {"veach-mis/mis_v4.pbrt","bathroom2/scene-v4.pbrt","cornell-box/scene-v4.pbrt"}
+do
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --fixed 1 --folder ${build}/MIS-Equal ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.99 --fixed 1 --folder ${build}/MIS-BRDF ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.01 --fixed 1 --folder ${build}/MIS-Light ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --folder ${build}/MIS-T0-01 --tsallis 0.01 ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --folder ${build}/MIS-T0-5 --tsallis 0.5 ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --folder ${build}/MIS-T1 --tsallis 1 ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --folder ${build}/MIS-T1-5 --tsallis 1.5 ../pbrt-v4-scenes-P3D/${scene}
+    ./${build}/pbrt --spp $nspp --batch $batch --nthreads ${numthreads} --alpha 0.5 --folder ${build}/MIS-T2 --tsallis 2 ../pbrt-v4-scenes-P3D/${scene}
+done
